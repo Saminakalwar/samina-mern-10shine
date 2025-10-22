93170da6b6ec2c2157f6c7efd413359d75996da0
Saminakalwar <saminakalwar9@gmail.com>
2025-10-22 16:44:41 +0500

    add Forgot Password modal, Reset Password page & updated related files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,24 @@ import Login from './pages/Login'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
+import ProtectedRoutes from './routes/ProtectedRoutes'
+import ForgotPasswordModal from './components/ForgotPasswordModal'
+import ResetPassword from './pages/ResetPassword'
 
 const App = () => {
   return (
    <Routes>
-
+    {/* Public Routes */}
     <Route path='/' element={<Signup/>}/>
     <Route path="/signup" element={<Signup />} />
     <Route path='/login' element={<Login/>}/>
-     <Route path='/dashboard' element={<Home/>}/>
-     <Route path="/profile" element={<Profile />} />
+    <Route path="/forgot-password" element={<ForgotPasswordModal />} />
+    <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+
+    {/* Protected Routes */}
+     <Route path='/dashboard' element={ <ProtectedRoutes> <Home/> <ProtectedRoutes/></ProtectedRoutes>}/>
+      <Route path="/profile" element={<ProtectedRoutes> <Profile /> </ProtectedRoutes>} />
 
    </Routes>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,46 +6,39 @@ import { validateEmail } from '../utils/helper';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import useAuth from '../hooks/useAuth';
+import ForgotPasswordModal from '../components/ForgotPasswordModal';
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+
     const {login} = useAuth(); //custom hook
     const navigate = useNavigate(); // hook
 
     const handleLogin = async(e)=>{
-        e.preventDefault(); //prevent the default behavior of form submission from reloading the page on user reclick
+      e.preventDefault(); //prevent the default behavior of form submission from reloading the page on user reclick
 
-    if (!email) {
-    setError("Please enter the email address.");
-    return;
-  }
-    if (!password) {
-    setError("Please enter the password.");
-    return;
-  }
+      if (!email) return setError("Please enter the email address.");
+      if (!password) return setError("Please enter the password.");
+      if (!validateEmail(email)) return setError("Please enter a valid email address");
 
-  if (!validateEmail(email)) {
-    setError("Please enter a valid email address");
-    return;
-  }
-  setError(null);
-  setLoading(true);
+      setError(null);
+      setLoading(true);
 
-  // Login API call
-  try{
-    await login(email, password);
-    navigate('/dashboard');
-  }
-  catch(err){
-   alert(err.response?.data?.message || 'Login failed');
-  }
-  finally {
-      setLoading(false);
-    }
-        
+      // Login API call
+      try{
+        await login(email, password);
+        navigate('/dashboard');
+      }
+      catch(err){
+      alert(err.response?.data?.message || 'Login failed');
+      }
+      finally {
+          setLoading(false);
+        }
     }
 
   return (
@@ -53,7 +46,10 @@ const Login = () => {
      <Navbar />
    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 bg-white rounded-2xl shadow-lg">
-        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+        <h2 className="text-2xl font-bold text-center mb-2">Login</h2>
+        <p className="text-center text-gray-500 mb-3">
+            Enter your credentials to access Notes App Dashboard.
+          </p>
 
         <form onSubmit={handleLogin} className="space-y-4">
           <input
@@ -68,72 +64,45 @@ const Login = () => {
             autoComplete="email"
           />
 
-          <PasswordInput id="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value) }  autoComplete="current-password"
-/>
+          <PasswordInput id="password" name="password" value={password} onChange={(e)=>setPassword(e.target.value) }  autoComplete="current-password"/>
 
           {error && <p className="text-red-500 text-sm">{error}</p>}
 
           <button
               type="submit"
-              className="btn-primary w-full py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition disabled:opacity-60"
+              className="btn-primary w-full py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition disabled:opacity-60"
               disabled={loading}
             >
               {loading ? "Logging in..." : "Login"}
+
             </button>
+
+             {/* 🔹 Forgot Password Link */}
+            <div className="text-right mt-0">
+              <button
+                type="button"
+                onClick={() => setOpenModal(true)}
+                className="text-blue-500 hover:underline text-sm"
+              >
+                Forgot password?
+              </button>
+            </div>
+
+        </form>
         
-        <p className="mt-6 text-sm text-center">
+        <p className="mt-4 text-sm text-center">
           Not registered yet?{" "}
           <Link to="/signup" className="text-primary hover:underline">
             Create an Account
           </Link>
         </p>
-        </form>
-
 
       </div>
     </div>
+
+    {/* 🔹 Forgot Password Modal */}
+      <ForgotPasswordModal open={openModal} onClose={() => setOpenModal(false)} />
     </>
   )
 }
-
 export default Login
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   try{
-//     const res = await API.post('/auth/login', {email, password});
-//     // handle successful login response
-//     if(res.data && res.data.token){
-//      const { token, user } = res.data;// from response data object it will only fetch the token & user
-//     localStorage.setItem("token", token);
-//     localStorage.setItem("user", JSON.stringify(user));
-//     navigate('/dashboard');
-//     }
-//     else {
-//         setError("Invalid login response.");
-//       }
-//   }
-//   catch(error){
-//     // handle Login Error
-// // if (error.response && error.response.data && error.response.data.message) { //or
-// if (error.response?.data?.message) {
-//   setError(error.response.data.message);
-// } else {
-//   setError("An unexpected error occurred. Please try again.");
-// }
